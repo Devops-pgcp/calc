d1d0136c5f9edcaf71a7686f6623e92e9ab094d8
Devops-pgcp <abhi11ghorpade@gmail.com>
2026-06-25 13:10:55 +0530

    modify file for substraction

--- addition.py
+++ addition.py
@@ -1,4 +1,9 @@
 a = int(input("Enter the first number: "))
 b = int(input("Enter the second number: "))
-c = a + b
-print("The sum is:", c)
+
+
+def subtract(x, y):
+    return x - y
+
+substraction_result = subtract(a, b)
+print("The result of subtraction is:", substraction_result)
